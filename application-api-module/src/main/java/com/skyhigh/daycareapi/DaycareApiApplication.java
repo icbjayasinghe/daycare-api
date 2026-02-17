32f70166db3b3152f19ec3279d50eadd58bf8862
Isuru <icbjayasinghe@gmail.com>
2026-02-17 06:11:04 -0400

    Add multimoduled project

---
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/DaycareApiApplication.java
@@ -0,0 +1,13 @@
+package com.skyhigh.daycareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaycareApiApplication.class, args);
+    }
+
+}
---
+++ application-api-module/src/test/java/com/skyhigh/daycareapi/DaycareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyhigh.daycareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaycareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
