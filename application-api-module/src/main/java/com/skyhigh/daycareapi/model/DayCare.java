feae3b7d6d7549e6f7fd8993c99ec0aefa4019ac
kavindasilva <kss.kavinda@gmail.com>
2026-02-17 20:31:00 -0500

    Basic db config added

---
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/model/DayCare.java
@@ -0,0 +1,39 @@
+package com.skyhigh.daycareapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DayCare")
+public class DayCare {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
