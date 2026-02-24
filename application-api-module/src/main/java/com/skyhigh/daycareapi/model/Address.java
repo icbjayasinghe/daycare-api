952d761b8232c222dc2b38b04322d02a09e2e0b9
Isuru <icbjayasinghe@gmail.com>
2026-02-24 06:17:46 -0400

    Add models

---
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/model/Address.java
@@ -0,0 +1,172 @@
+package com.skyhigh.daycareapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.annotation.Generated;
+import java.util.Objects;
+
+/**
+ * Address model
+ */
+
+@Schema(name = "Address", description = "Address model")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-02-24T05:34:36.063118-04:00[America/Halifax]")
+public class Address {
+
+  @JsonProperty("street")
+  private String street;
+
+  @JsonProperty("city")
+  private String city;
+
+  @JsonProperty("state")
+  private String state;
+
+  @JsonProperty("postalCode")
+  private String postalCode;
+
+  @JsonProperty("country")
+  private String country;
+
+  public Address street(String street) {
+    this.street = street;
+    return this;
+  }
+
+  /**
+   * Get street
+   * @return street
+  */
+  
+  @Schema(name = "street", required = false)
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public Address city(String city) {
+    this.city = city;
+    return this;
+  }
+
+  /**
+   * Get city
+   * @return city
+  */
+  
+  @Schema(name = "city", required = false)
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public Address state(String state) {
+    this.state = state;
+    return this;
+  }
+
+  /**
+   * Get state
+   * @return state
+  */
+  
+  @Schema(name = "state", required = false)
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public Address postalCode(String postalCode) {
+    this.postalCode = postalCode;
+    return this;
+  }
+
+  /**
+   * Get postalCode
+   * @return postalCode
+  */
+  
+  @Schema(name = "postalCode", required = false)
+  public String getPostalCode() {
+    return postalCode;
+  }
+
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
+
+  public Address country(String country) {
+    this.country = country;
+    return this;
+  }
+
+  /**
+   * Get country
+   * @return country
+  */
+  
+  @Schema(name = "country", required = false)
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Address address = (Address) o;
+    return Objects.equals(this.street, address.street) &&
+        Objects.equals(this.city, address.city) &&
+        Objects.equals(this.state, address.state) &&
+        Objects.equals(this.postalCode, address.postalCode) &&
+        Objects.equals(this.country, address.country);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(street, city, state, postalCode, country);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AddressDto {\n");
+    sb.append("    street: ").append(toIndentedString(street)).append("\n");
+    sb.append("    city: ").append(toIndentedString(city)).append("\n");
+    sb.append("    state: ").append(toIndentedString(state)).append("\n");
+    sb.append("    postalCode: ").append(toIndentedString(postalCode)).append("\n");
+    sb.append("    country: ").append(toIndentedString(country)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
