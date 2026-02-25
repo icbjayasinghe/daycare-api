22f0c02584835a069b9dde6e90e462a9e442d7bf
kavindasilva <kss.kavinda@gmail.com>
2026-02-25 12:44:48 -0500

    Fixed the merge confilcts

--- application-api-module/src/main/java/com/skyhigh/daycareapi/model/Address.java
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/model/Address.java
@@ -1,65 +1,60 @@
 package com.skyhigh.daycareapi.model;
 
-<<<<<<< HEAD
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
 
-import javax.annotation.Generated;
-import java.util.Objects;
-
-/**
- * Address model
- */
-
-@Schema(name = "Address", description = "Address model")
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2026-02-24T05:34:36.063118-04:00[America/Halifax]")
+@Entity
+@Table(name = "Address")
 public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  @JsonProperty("street")
-  private String street;
+  private String streetNumber;
 
-  @JsonProperty("city")
+  private String addressLine1;
+
+  private String addressLine2;
+
   private String city;
 
-  @JsonProperty("state")
-  private String state;
+  private String province;
 
-  @JsonProperty("postalCode")
   private String postalCode;
 
-  @JsonProperty("country")
-  private String country;
+  private String location;
 
-  public Address street(String street) {
-    this.street = street;
-    return this;
+  public Long getId() {
+    return id;
   }
 
-  /**
-   * Get street
-   * @return street
-  */
-  
-  @Schema(name = "street", required = false)
-  public String getStreet() {
-    return street;
+  public void setId(Long id) {
+    this.id = id;
   }
 
-  public void setStreet(String street) {
-    this.street = street;
+  public String getStreetNumber() {
+    return streetNumber;
   }
 
-  public Address city(String city) {
-    this.city = city;
-    return this;
+  public void setStreetNumber(String streetNumber) {
+    this.streetNumber = streetNumber;
   }
 
-  /**
-   * Get city
-   * @return city
-  */
-  
-  @Schema(name = "city", required = false)
+  public String getAddressLine1() {
+    return addressLine1;
+  }
+
+  public void setAddressLine1(String addressLine1) {
+    this.addressLine1 = addressLine1;
+  }
+
+  public String getAddressLine2() {
+    return addressLine2;
+  }
+
+  public void setAddressLine2(String addressLine2) {
+    this.addressLine2 = addressLine2;
+  }
+
   public String getCity() {
     return city;
   }
@@ -68,36 +63,14 @@ public class Address {
     this.city = city;
   }
 
-  public Address state(String state) {
-    this.state = state;
-    return this;
+  public String getProvince() {
+    return province;
   }
 
-  /**
-   * Get state
-   * @return state
-  */
-  
-  @Schema(name = "state", required = false)
-  public String getState() {
-    return state;
+  public void setProvince(String province) {
+    this.province = province;
   }
 
-  public void setState(String state) {
-    this.state = state;
-  }
-
-  public Address postalCode(String postalCode) {
-    this.postalCode = postalCode;
-    return this;
-  }
-
-  /**
-   * Get postalCode
-   * @return postalCode
-  */
-  
-  @Schema(name = "postalCode", required = false)
   public String getPostalCode() {
     return postalCode;
   }
@@ -106,157 +79,11 @@ public class Address {
     this.postalCode = postalCode;
   }
 
-  public Address country(String country) {
-    this.country = country;
-    return this;
+  public String getLocation() {
+    return location;
   }
 
-  /**
-   * Get country
-   * @return country
-  */
-  
-  @Schema(name = "country", required = false)
-  public String getCountry() {
-    return country;
-  }
-
-  public void setCountry(String country) {
-    this.country = country;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Address address = (Address) o;
-    return Objects.equals(this.street, address.street) &&
-        Objects.equals(this.city, address.city) &&
-        Objects.equals(this.state, address.state) &&
-        Objects.equals(this.postalCode, address.postalCode) &&
-        Objects.equals(this.country, address.country);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(street, city, state, postalCode, country);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class AddressDto {\n");
-    sb.append("    street: ").append(toIndentedString(street)).append("\n");
-    sb.append("    city: ").append(toIndentedString(city)).append("\n");
-    sb.append("    state: ").append(toIndentedString(state)).append("\n");
-    sb.append("    postalCode: ").append(toIndentedString(postalCode)).append("\n");
-    sb.append("    country: ").append(toIndentedString(country)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+  public void setLocation(String location) {
+    this.location = location;
   }
 }
-
-=======
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "Address")
-public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String streetNumber;
-
-    private String addressLine1;
-
-    private String addressLine2;
-
-    private String city;
-
-    private String province;
-
-    private String postalCode;
-
-    private String location;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(String streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-}
->>>>>>> adbc208 (Created basic DB structure wth main entities)
