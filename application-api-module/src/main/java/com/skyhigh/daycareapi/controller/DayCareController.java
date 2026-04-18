b47f0571395dbdef442361cbe5f68eb0bbfc5484
kavindasilva <kss.kavinda@gmail.com>
2026-04-18 19:00:15 -0400

    daycare api generated

---
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/controller/DayCareController.java
@@ -0,0 +1,182 @@
+package com.skyhigh.daycareapi.controller;
+
+import com.skyhigh.daycareapi.model.dto.DayCareDto;
+import com.skyhigh.daycareapi.model.facility.DayCare;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import java.util.*;
+
+@Validated
+@Tag(name = "daycare", description = "Daycare operations")
+public interface DayCareController {
+    default Optional<NativeWebRequest> getRequest() {
+        return Optional.empty();
+    }
+
+    @Operation(
+            operationId = "createDayCare",
+            summary = "Create new daycare",
+            tags = {"Daycare"},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Successful operation", content = {
+                        @Content(mediaType = "application/json", schema = @Schema(implementation = DayCareDto.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Operation failed")
+            }
+    )
+    @RequestMapping(
+            method = {RequestMethod.POST},
+            value = {"/daycare"},
+            produces = {"application/json"},
+            consumes = {"application/json"}
+    )
+    default ResponseEntity<DayCareDto> createDayCare(@Parameter(name = "DayCareDto",description = "Created daycare object",required = true) @RequestBody @Valid DayCareDto dayCareDto) {
+        this.getRequest().ifPresent((request) -> {
+            for(MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"address\" : { \"country\" : \"country\", \"city\" : \"city\", \"street\" : \"street\", \"postalCode\" : \"postalCode\", \"state\" : \"state\" }, \"name\" : \"name\", \"telephone\" : \"telephone\", \"owners\" : [ { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" }, { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" } ] }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+
+        });
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Operation(
+            operationId = "deleteDayCare",
+            summary = "Delete daycare by id",
+            tags = {"Daycare"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation",
+                        content = {@Content(mediaType = "application/json", schema = @Schema(implementation = DayCareDto.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Operation failed")
+            }
+    )
+    @RequestMapping(
+            method = {RequestMethod.DELETE},
+            value = {"/daycare/{id}"},
+            produces = {"application/json"}
+    )
+    default ResponseEntity<DayCareDto> deleteDayCare(@Parameter(name = "id",description = "Numeric ID of the daycare to delete",required = true) @PathVariable("id") Integer id) {
+        this.getRequest().ifPresent((request) -> {
+            for(MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"address\" : { \"country\" : \"country\", \"city\" : \"city\", \"street\" : \"street\", \"postalCode\" : \"postalCode\", \"state\" : \"state\" }, \"name\" : \"name\", \"telephone\" : \"telephone\", \"owners\" : [ { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" }, { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" } ] }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+
+        });
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Operation(
+            operationId = "editDayCare",
+            summary = "Edit daycare",
+            tags = {"Daycare"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = DayCareDto.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Operation failed")
+            }
+    )
+    @RequestMapping(
+            method = {RequestMethod.PUT},
+            value = {"/daycare"},
+            produces = {"application/json"},
+            consumes = {"application/json"}
+    )
+    default ResponseEntity<DayCareDto> editDayCare(@Parameter(name = "DayCareDto",description = "Created daycare object",required = true) @RequestBody @Valid DayCareDto dayCareDto) {
+        this.getRequest().ifPresent((request) -> {
+            for(MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"address\" : { \"country\" : \"country\", \"city\" : \"city\", \"street\" : \"street\", \"postalCode\" : \"postalCode\", \"state\" : \"state\" }, \"name\" : \"name\", \"telephone\" : \"telephone\", \"owners\" : [ { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" }, { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" } ] }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+
+        });
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Operation(
+            operationId = "getDayCare",
+            summary = "Get daycare by id",
+            tags = {"Daycare"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = DayCareDto.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Operation failed")
+            }
+    )
+    @RequestMapping(
+            method = {RequestMethod.GET},
+            value = {"/daycare/{id}"},
+            produces = {"application/json"}
+    )
+    default ResponseEntity<DayCareDto> getDayCare(@Parameter(name = "id",description = "Numeric ID of the daycare to delete",required = true) @PathVariable("id") Integer id) {
+        this.getRequest().ifPresent((request) -> {
+            for(MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"address\" : { \"country\" : \"country\", \"city\" : \"city\", \"street\" : \"street\", \"postalCode\" : \"postalCode\", \"state\" : \"state\" }, \"name\" : \"name\", \"telephone\" : \"telephone\", \"owners\" : [ { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" }, { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" } ] }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+
+        });
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Operation(
+            operationId = "listDayCares",
+            summary = "Get daycare list",
+            tags = {"Daycare"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = DayCareDto.class))
+                    }),
+                    @ApiResponse(responseCode = "400", description = "Operation failed")
+            }
+    )
+    @RequestMapping(
+            method = {RequestMethod.GET},
+            value = {"/daycare"},
+            produces = {"application/json"}
+    )
+    default ResponseEntity<DayCareDto> listDayCares(@Parameter(name = "keyword",description = "Search keyword (searches name, description, etc.)") @RequestParam(value = "keyword",required = false) @Valid String keyword, @Parameter(name = "latitude",description = "Latitude of the center point") @RequestParam(value = "latitude",required = false) @DecimalMin("-90") @DecimalMax("90") @Valid Float latitude, @Parameter(name = "longitude",description = "Longitude of the center point") @RequestParam(value = "longitude",required = false) @DecimalMin("-180") @DecimalMax("180") @Valid Float longitude, @Parameter(name = "radius",description = "Search radius in kilometers from the center point") @RequestParam(value = "radius",required = false,defaultValue = "10") @DecimalMin("0.1") @DecimalMax("100") @Valid Float radius) {
+        this.getRequest().ifPresent((request) -> {
+            for(MediaType mediaType : MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"address\" : { \"country\" : \"country\", \"city\" : \"city\", \"street\" : \"street\", \"postalCode\" : \"postalCode\", \"state\" : \"state\" }, \"name\" : \"name\", \"telephone\" : \"telephone\", \"owners\" : [ { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" }, { \"firstName\" : \"firstName\", \"lastName\" : \"lastName\", \"phoneNumber\" : \"phoneNumber\", \"email\" : \"email\" } ] }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+
+        });
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+}
---
+++ application-api-module/src/main/java/com/skyhigh/daycareapi/model/dto/OwnerDto.java
@@ -0,0 +1,109 @@
+package com.skyhigh.daycareapi.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.Valid;
+
+public class OwnerDto {
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("phoneNumber")
+    private String phoneNumber;
+
+    // TODO: use mapstruct for enums
+    @JsonProperty("userType")
+    private int userType;
+
+    public OwnerDto firstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    @Schema(
+            name = "firstName",
+            required = false
+    )
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public OwnerDto lastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    @Schema(
+            name = "lastName",
+            required = false
+    )
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public OwnerDto email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Schema(
+            name = "email",
+            required = false
+    )
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OwnerDto phoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    @Schema(
+            name = "phoneNumber",
+            required = false
+    )
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public OwnerDto userType(int userType) {
+        this.userType = userType;
+        return this;
+    }
+
+    @Schema(
+            name = "userType",
+            required = false
+    )
+    public @Valid int getUserType() {
+        return this.userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+}
